4d77947f48a2d85d9d358dbabdc60a6db11c0dd4
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-07-26 12:26:32 +0000

    Make artist page responsive

--- src/pages/korsah.js
+++ src/pages/korsah.js
@@ -12,7 +12,28 @@ function Korsah() {
             <img src={korsahbw} alt="korsahwh" />
           </div>
           <div className={styles.blue}>
-            <h1>Biography</h1>
+            <h4>Korsah</h4>
+            <div className={styles.biography}>
+              <h5>Biography</h5>
+              <p>
+                Born at Atonsu in the capital of the Ashanti region of Ghana,
+                Kumasi, Owusu Nkwantabisa well known by his stage name Korsah is
+                an Afrobeat and Hip-Hop artist. He was born to Atta Korsah and
+                Lydia Kunadu on April 12, 1995. The artist was inspired to go
+                into music soon after he lost his mother in 2015.
+              </p>
+              <p>
+                The event in 2015 was devastating, but he drew inspiration from
+                it. He moved to Accra where he found his love for music. As a
+                young boy, he grew to appreciate Ghanaian music but decided to
+                add a new dimension to it when he got the chance.
+              </p>
+              <p>
+                When Korsah decided to go into music, he took time to understudy
+                music and decided on his genre afterwards. Since then, he has
+                been an admirable figure to many.
+              </p>
+            </div>
           </div>
         </section>
       </Layout>
