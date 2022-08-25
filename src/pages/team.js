9ffdb7358ff23aedbe3ef5d04f045b0151d7bfdf
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-08-25 03:58:57 +0000

    fix blurry image on iphones

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout"
 import * as styles from "../components/styles/team.module.css"
 import ella from "../components/images/ELLA.jpg"
 import emma from "../components/images/EMMA.jpg"
-import flow from "../components/images/FLOW.jpg"
+import sony from "../components/images/sony.jpg"
 import tef from "../components/images/TEF.jpg"
 
 function Team() {
@@ -13,6 +13,36 @@ function Team() {
         <section className={styles.team}>
           <h2>Meet The 610 Team</h2>
           <div className={styles.crew}>
+            <div className={styles.crewCard}>
+              <div className={styles.imgContainer}>
+                <img src={sony} alt="Flow" />
+              </div>
+              <div className={styles.connect}>
+                <h3>Kofi Sony</h3>
+              </div>
+              <div className={styles.overview}>
+                With a career spanning over 15 years, Kofi Sonny leads 610Music,
+                a record label he founded in 2010. As an executive producer,
+                Kofi Sonny has always had a knack for pushing emerging artists
+                and breaking popular local artists into mainstream pop culture.
+                <div className="social">
+                  <a
+                    href="https://twitter.com/610music"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/610music/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
             <div className={styles.crewCard}>
               <div className={styles.imgContainer}>
                 <img src={tef} alt="tef" />
@@ -105,65 +135,6 @@ function Team() {
                 </div>
               </div>
             </div>
-            <div className={styles.crewCard}>
-              <div className={styles.imgContainer}>
-                <img src={flow} alt="Flow" />
-              </div>
-              <div className={styles.connect}>
-                <h3>Rich flow</h3>
-              </div>
-              <div className={styles.overview}>
-                Rich Flow is an American rapper, singer, and songwriter signed
-                to 610MUSIC. After over a decade as a recording artist, Rich
-                Flow began gaining recognition in 2015.
-                <div className="social">
-                  <a
-                    href="https://twitter.com/610music"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="bi bi-twitter"></i>
-                  </a>
-                  <a
-                    href="https://www.instagram.com/610music/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="bi bi-instagram"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div className={styles.crewCard}>
-              <div className={styles.imgContainer}>
-                <img src={sony} alt="Sony" />
-              </div>
-              <div className={styles.connect}>
-                <h3>Sony</h3>
-              </div>
-              <div className={styles.overview}>
-                With a career spanning over 15 years, Kofi Sonny leads 610Music,
-                a record label he founded in 2010. As an executive producer,
-                Kofi Sonny has always had a knack for pushing emerging artists
-                and breaking popular local artists into mainstream pop culture.
-                <div className="social">
-                  <a
-                    href="https://twitter.com/610music"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="bi bi-twitter"></i>
-                  </a>
-                  <a
-                    href="https://www.instagram.com/610music/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="bi bi-instagram"></i>
-                  </a>
-                </div>
-              </div>
-            </div> */}
           </div>
         </section>
       </Layout>
